b81ba9d6259075dad1d5633961c7e3a565635a92
ErickFraga <37560574+ErickFraga@users.noreply.github.com>
2018-05-20 13:40:37 -0300

    0.1
    Consegui, faer as letras certas aparecerem na tela quado clickadas. O proximo passo e conseguir fazer elas não se repetirem e aparecerem em ordem.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Meu objetivo com este projeto e criar uma interface com um teclado contendo as l
 um placar de pontos para o usuario, um contador de tentativas falhas (maximo 7) e 
 exibir letra por letra da plavra-chave de acordo com a escolha do usuario.
 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+---------------------------------------------------------------------------
 
 My goal with this design and create an interface with a keyboard containing the letters that the user can choose,
 a scoreboard for the user, a counter of failed attempts (maximum 7) 
--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 var Letra;
+var palavra_secreta;
+var Banco_de_palavras;
 window.onload = function init() {
 var teclado = document.getElementById("teclado");
+var palavrachave = document.getElementById("palavrachave");
+var letrachave = document.getElementById("letrachave");
 var i;
 
+ Banco_de_palavras = ['mouse', 'marrom', 'porta', 'massa', 'palha', 'pupila']
+var rand = Math.floor(Math.random() * Banco_de_palavras.length);
+palavra_secreta = Banco_de_palavras[rand].toUpperCase().split("");
+
+for(i=0; i<palavra_secreta.length; i++){
+    palavrachave.innerHTML += "<c id='ltr" + i + "'>" + palavra_secreta[i] + "</c>";
+}
+
+/*for(i = 0; i < palavra_secreta.length; i++){
+    letrachave.innerHTML += "<c id='ltr" + i + "></c>";
+}*/
+
+//Gerador do Teclado
 letra = new Array('Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M');
 
     for(i = 0; i < letra.length; i++){
-        teclado.innerHTML += "<button class='tecla' onclick='Chute(" + letra[i] + ")'>" + letra[i] + "</button>";
+        var lString = "Chute('" + letra[i] + "')";
+        teclado.innerHTML += "<button class='tecla' onclick=" + lString + ">" + letra[i] + "</button>";
     }
 
 };
 
-function Chute(a){
-    console.log(a);
+function Chute(lrt){
+    for(var j = 0; j < palavra_secreta.length; j++){
+        if(lrt == palavra_secreta[j]){
+            document.getElementById("palavrachave").innerHTML += "<c>" + palavra_secreta[j] + "</c>"
+        }
+    }
+
+    
 }
+
