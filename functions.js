7c2ef8601f35e7642e33860dbdd84caee869a8aa
ErickFraga <37560574+ErickFraga@users.noreply.github.com>
2018-05-21 21:01:38 -0300

    0.2
    Consegui faer fuincionar \O/

--- functions.js
+++ functions.js
@@ -1,3 +1,50 @@
 
+function Perdeu(){
+    document.getElementById("resultado").innerHTML = "<div id='lose'>VOCE PERDEU!</div>";
+    fim_de_jogo = true;
+    for(i=0; i<palavra_secreta.length; i++){
+        document.getElementById("ltr" + i).style.visibility = "visible";
+    }
+}
+
+
+function Ganhou(){
+    document.getElementById("resultado").innerHTML = "<div id='win'>VOCE GANHOU!</div>"
+    
+    fim_de_jogo = true;
+}
+
+
+function Chute(ltr){
+    //Verifica se o jogo acabou "Se o Jogo Não tiver acabado" (linha: 45-55).            
+    if(fim_de_jogo == false){   
+        //Procura pela letra clickada e muda a cor do background do botão (linha: 47-55).
+        for(var j = 0; j < palavra_secreta.length; j++){
+            if((ltr == palavra_secreta[j])&&(erros < 7)){
+                var idltr = "ltr" + j;
+                document.getElementById(idltr).style.visibility = "visible";
+                document.getElementById(idltr).style.color = "green";
+                document.getElementById(ltr).style.backgroundColor = "green";
+                acertos+=1;
+                valid = true;
+                
+            };
+        };
+        //Confere fora da Iteração se a letra esta errada (linha: 58-61).
+        if((valid == false)&&(erros <= 7)){
+            document.getElementById(ltr).style.backgroundColor = "red";
+            erros+=1;
+        }; if((valid == false)&&(erros >= 7)){   //Confere se o imite de erros foi ultrapassdo (linha: 61-63).
+            Perdeu()
+        }else if(acertos == palavra_secreta.length){    //Confere se todas as letras ja foram exibidas (linha: 63-65).
+            Ganhou();
+        };
+    };
+    valid = false;
+    document.getElementById("falhas").innerHTML = erros;
+};
+
+
+
 
 
--- main.js
+++ main.js
@@ -1,47 +1,35 @@
-var Letra;
+var Letras;
 var palavra_secreta;
 var Banco_de_palavras;
-window.onload = function init() {
+window.onload = function init() {  //chama a funcão quando a janela e carregada(linha: 4 - 28)
 var teclado = document.getElementById("teclado");
 var palavrachave = document.getElementById("palavrachave");
 var letrachave = document.getElementById("letrachave");
 var i;
 
+palavrachave.style.color = "red";
+
  Banco_de_palavras = ['mouse', 'marrom', 'porta', 'massa', 'palha', 'pupila']
 var rand = Math.floor(Math.random() * Banco_de_palavras.length);
 palavra_secreta = Banco_de_palavras[rand].toUpperCase().split("");
 
-for(i=0; i<palavra_secreta.length; i++){
+for(i=0; i<palavra_secreta.length; i++){ //Exibe um bloco vazio para cada letra da palavra secreta
     var escondido = "style='visibility:hidden;'";
     palavrachave.innerHTML += "<cor class='key'><char " + escondido + " id='ltr" + i + "'>" + palavra_secreta[i] + "</char><cor";
 }
 
 //Gerador do Teclado
-letra = new Array('Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M');
+letras = new Array('Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M');
 
-    for(i = 0; i < letra.length; i++){
-        var lString = "Chute('" + letra[i] + "')";
-        var idtecla = "id='" + letra[i] + "'";
-        teclado.innerHTML += "<button class='tecla' " + idtecla + " onclick=" + lString + ">" + letra[i] + "</button>";
+    for(i = 0; i < letras.length; i++){
+        var lString = "Chute('" + letras[i] + "')";
+        var idtecla = "id='" + letras[i] + "'";
+        teclado.innerHTML += "<button class='tecla' " + idtecla + " onclick=" + lString + ">" + letras[i] + "</button>";
     }
 
 };
-var cont = 0;
-function Chute(ltr){
-    for(var j = 0; j < palavra_secreta.length; j++){
-        if(ltr == palavra_secreta[j]){
-            var idltr = "ltr" + j;
-            document.getElementById(idltr).style.visibility = "visible";
-            document.getElementById(ltr).style.backgroundColor = "green";
-            
-            
-             cont += 1;
-            
-            //document.getElementById("palavrachave").innerHTML += "<c>" + palavra_secreta[j] + "</c>"
-        }else if(cont == 0){
-            document.getElementById(ltr).style.backgroundColor = "red";
-        }
-    }
 
-   cont = 0; 
-}
+var fim_de_jogo = false;    //Confirma se o jogo acabou ou não (linhas 32, 37, 45).
+var valid = false;   //Serve para validar se não ouveram acertos na iteração (linhas 53, 58, 67).
+var erros = 0;  //Contador de erros (linhas 48, 58, 60, 61).
+var acertos = 0;    //Contadro de acertos (linhas 52, 63).
